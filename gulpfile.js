7e37993741ab3b523999e8a274a3da287a496a91
Mark Chen <markchen@Marks-Studio.local>
2017-01-24 18:35:25 -0800

    Make Website Responsive and minify all the files

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
 var gulp = require('gulp'),
 	htmlmin = require('gulp-htmlmin'), // For minifying HTML files
 	minifyCss = require('gulp-minify-css'), // For minifying CSS files
-	uglify = require('gulp-uglify'),  // For minifying JS files
-	plumber = require('gulp-plumber') // To stop pipe from breaking in the event of an error
+	uglify = require('gulp-uglify'), // For minifying JS files
+	plumber = require('gulp-plumber'); // To stop pipe from breaking in the event of an error
 
 gulp.task('views', function() { // Task to run "views" methods
-	gulp.src('app/*.html') // File(s) source
-	.pipe(plumber())
-	.pi;e(htmlmin({collapseWhitespace:true}))
-	.pipe(gulp.dest('public')) // File(s) destination
+  	gulp.src('app/*.html') // File(s) source
+  		.pipe(plumber())
+    	.pipe(htmlmin({collapseWhitespace: true}))
+    	.pipe(gulp.dest('public')) // File(s) destination
 });
 
 gulp.task('styles', function() { // Task to run "styles" methods
-	gulp.src(['app/css/*.css']) // File(s) source
-	.pipe(plumber())
-	.pipe(minifyCss({compatability: 'ie8'}))
-	.pipe(gulp.dest('public/css')) // File(s) destination
+  	gulp.src(['app/css/*.css']) // File(s) source
+  		.pipe(plumber())
+    	.pipe(minifyCss({compatibility: 'ie8'}))
+    	.pipe(gulp.dest('public/css')) // File(s) destination
 });
 
 gulp.task('scripts', function() { // Task to run "scripts" methods
